85b17b131a354046c42095626f7171bbacc1b76c
Lee424 <tmdghks424@naver.com>
2018-12-23 15:29:23 +0900

    구글 지도 수정

--- Travely_iOS/ViewController.swift
+++ Travely_iOS/ViewController.swift
@@ -13,28 +13,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loadGoogleMap()
     }
     
-
-    
-    override func loadView() {
+    func loadGoogleMap() {
         // 구글지도
         let lat: CLLocationDegrees = 37.558514
         let long: CLLocationDegrees = 126.925239
-
-
+        
+        
         let pos = GMSCameraPosition.camera(withLatitude: lat, longitude: long, zoom: 15)
         //        let mapView = GMSMapView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: pos)
+        let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height), camera: pos)
         mapView.isMyLocationEnabled = true
-        view = mapView
-
+        self.view.addSubview(mapView)
+        
         // 구글지도 마커 표시
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: lat, longitude: long)
         marker.title = "홍대입구 근처~~"
         marker.map = mapView
     }
+
+    
 }
 
