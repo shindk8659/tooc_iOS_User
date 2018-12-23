a9b236ed3e3d9a802fb04fe55f005ebecc425fc4
신동규 <shindk8659@naver.com>
2018-12-23 14:12:01 +0900

    프로젝트 추가

---
+++ Travely_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travely_iOS
+//
+//  Created by 신동규 on 12/23/18.
+//  Copyright © 2018 신동규. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
