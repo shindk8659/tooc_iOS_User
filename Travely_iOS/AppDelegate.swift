40ec9d9ccd87d3aba340b3239ebb52978821798d
Lee424 <tmdghks424@naver.com>
2018-12-23 15:20:15 +0900

    구글 지도 추가

--- Travely_iOS/AppDelegate.swift
+++ Travely_iOS/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    let googleMapsApiKey = "AIzaSyAPalGrZ8ZT8oXCBNEkTOjzFaF8LLig_Mo"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GMSServices.provideAPIKey(googleMapsApiKey)
         return true
     }
 
--- Travely_iOS/ViewController.swift
+++ Travely_iOS/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+
+    
+    override func loadView() {
+        // 구글지도
+        let lat: CLLocationDegrees = 37.558514
+        let long: CLLocationDegrees = 126.925239
 
 
+        let pos = GMSCameraPosition.camera(withLatitude: lat, longitude: long, zoom: 15)
+        //        let mapView = GMSMapView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: pos)
+        mapView.isMyLocationEnabled = true
+        view = mapView
+
+        // 구글지도 마커 표시
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        marker.title = "홍대입구 근처~~"
+        marker.map = mapView
+    }
 }
 
