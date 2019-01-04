f6484c893a5fe5ca158313b751b4191ab7d6b29e
신동규 <shindk8659@naver.com>
2019-01-04 20:33:48 +0900

    프로젝트 폴더링 추가 및 마이페이지 시작

---
+++ Travely_iOS/MainView/MyPage/MyPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyPageViewController.swift
+//  Travely_iOS
+//
+//  Created by 신동규 on 1/4/19.
+//  Copyright © 2019 신동규. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
