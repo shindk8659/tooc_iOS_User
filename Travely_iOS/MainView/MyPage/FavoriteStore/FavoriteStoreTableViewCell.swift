9f8ed93f1d5b2c1bc381f3c8b463027aa9e7a4bf
신동규 <shindk8659@naver.com>
2019-01-05 03:37:20 +0900

    마이페이지 즐겨찾기 리뷰 뷰 완성 및 디자인 적용완료

---
+++ Travely_iOS/MainView/MyPage/FavoriteStore/FavoriteStoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteStoreTableViewCell.swift
+//  Travely_iOS
+//
+//  Created by 신동규 on 1/5/19.
+//  Copyright © 2019 신동규. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteStoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
