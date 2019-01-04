91076de503057e4f1fb3506aaafb879f158c85e8
신동규 <shindk8659@naver.com>
2019-01-04 23:26:51 +0900

    마이페이지 뷰 완성 및 디자인 완료

--- Travely_iOS/MainView/MyPage/MyPageViewController.swift
+++ Travely_iOS/MainView/MyPage/MyPageViewController.swift
@@ -10,21 +10,37 @@ import UIKit
 
 class MyPageViewController: UIViewController {
 
+    @IBOutlet weak var myPageTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.myPageTableView.delegate = self
+        self.myPageTableView.dataSource = self
+        
+    self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.backgroundColor = UIColor.white
 
         // Do any additional setup after loading the view.
     }
+  
+
+}
+extension MyPageViewController: UITableViewDelegate
+{
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension MyPageViewController: UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "recentstoragecell") as! RecentStorageTableViewCell
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none;
+        cell.separatorInset = UIEdgeInsets.zero
+        return cell
+    }
+    
+    
 }
---
+++ Travely_iOS/MainView/MyPage/RecentStorageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecentStorageTableViewCell.swift
+//  Travely_iOS
+//
+//  Created by 신동규 on 1/4/19.
+//  Copyright © 2019 신동규. All rights reserved.
+//
+
+import UIKit
+
+class RecentStorageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
