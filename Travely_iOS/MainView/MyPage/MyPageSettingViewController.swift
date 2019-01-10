08d0aac720a452622e3aabcbeacef0f01f3c5d17
신동규 <shindk8659@naver.com>
2019-01-10 19:56:32 +0900

    앱가이드 연결 및 인포피리스트 파일 수정

--- Travely_iOS/MainView/MyPage/MyPageSettingViewController.swift
+++ Travely_iOS/MainView/MyPage/MyPageSettingViewController.swift
@@ -17,6 +17,14 @@ class MyPageSettingViewController: UITableViewController {
         languageView.layer.borderWidth = 1
         languageView.layer.borderColor = UIColor.lightGray.cgColor
     }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+        self.navigationController?.navigationBar.barTintColor = UIColor.white
+        self.navigationController?.navigationBar.setBackgroundImage(nil, for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.backgroundColor = UIColor.white
+        self.tabBarController?.hideTabBarAnimated(hide: false)
+    }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch indexPath.row {
@@ -24,7 +32,11 @@ class MyPageSettingViewController: UITableViewController {
             let VC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "aboutTooc")
             VC.addBackButton("black")
             self.navigationController?.pushViewController(VC, animated: true)
-        case 3: return
+        case 3:
+            let VC = UIStoryboard.init(name: "LoginSignup", bundle: nil).instantiateViewController(withIdentifier: "appguidepageview")
+             self.tabBarController?.hideTabBarAnimated(hide: true)
+             self.navigationController?.pushViewController(VC, animated: true)
+            VC.addBackButton("black")
         case 4:
             let VC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "faqview") as! FAQViewController
             VC.addBackButton("black")
